a22117e5882d6b2c5c8716420b6ebd45b04db192
wectazz <wectazz@gmail.com>
2026-05-02 17:31:03 +0300

    Update check_file.py

--- check_file.py
+++ check_file.py
@@ -6,9 +6,10 @@ import hashlib
 URL = "https://raw.githubusercontent.com/Wuang26/Kaorios-Toolbox/refs/heads/main/Toolbox-data/Keybox.xml"
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CAPTION = "Keybox 🟢🟢🟢"
+FILENAME = "Keybox.xml"
+HASH_FILE = "last_hash.txt"
 
-# Список целей для отправки
-# Если TELEGRAM_THREAD_ID_2 не задан, он будет None
+# Список чатов (Первая группа и Вторая группа с темой)
 TARGETS = [
     {
         "chat_id": os.getenv("TELEGRAM_CHAT_ID"),
@@ -21,8 +22,6 @@ TARGETS = [
         "msg_id_file": "last_msg_id_2.txt"
     }
 ]
-
-HASH_FILE = "last_hash.txt"
 
 def get_hash(text):
     return hashlib.sha256(text.encode('utf-8')).hexdigest()
@@ -38,29 +37,32 @@ def delete_old_message(target):
 def send_to_target(target, content):
     delete_old_message(target)
     
-    filename = "Keybox.xml"
-    with open(filename, "w", encoding="utf-8") as f:
+    # Сохраняем файл
+    with open(FILENAME, "w", encoding="utf-8") as f:
         f.write(content)
     
     send_url = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
-    with open(filename, "rb") as f:
+    with open(FILENAME, "rb") as f:
         payload = {
             "chat_id": target["chat_id"],
             "caption": CAPTION,
             "parse_mode": "HTML"
         }
-        # Если указан thread_id, добавляем его в запрос
-        if target["thread_id"]:
-            payload["message_thread_id"] = target["thread_id"]
+        # Проверка на ID темы
+        t_id = target.get("thread_id")
+        if t_id and str(t_id).strip():
+            payload["message_thread_id"] = t_id
             
         files = {"document": f}
         r = requests.post(send_url, data=payload, files=files)
         
         if r.status_code == 200:
-            new_msg_id = r.json().get("result", {}).get("message_id")
+            new_id = r.json().get("result", {}).get("message_id")
             with open(target["msg_id_file"], "w") as f:
-                f.write(str(new_msg_id))
-            print(f"Отправлено в {target['chat_id']}, тема: {target['thread_id']}")
+                f.write(str(new_id))
+            print(f"Отправлено в {target['chat_id']}")
+        else:
+            print(f"Ошибка TG ({target['chat_id']}): {r.text}")
 
 # Логика
 response = requests.get(URL)
@@ -75,13 +77,15 @@ if response.status_code == 200:
         last_hash = ""
 
     if current_hash != last_hash:
-        print("Обновление!")
+        print("Найдено обновление!")
         for target in TARGETS:
-            if target["chat_id"]: # Проверяем, что ID группы вообще задан
+            if target["chat_id"]: # Проверка, что секрет заполнен
                 send_to_target(target, current_content)
         
         with open(HASH_FILE, "w") as f:
             f.write(current_hash)
     else:
         print("Изменений нет.")
-        
+else:
+    print(f"Ошибка загрузки: {response.status_code}")
+    
